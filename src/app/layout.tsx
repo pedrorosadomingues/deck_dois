da94b8dc1dd33fd63b163ffe4ab614c59640ef11
Pedro Rosa <pedrorosadomingues@gmail.com>
2024-06-06 14:29:18 -0300

    Update title in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Deck 2",
+  title: "Deck Rio Sao Bartolomeu 2",
   description: "Aplicacao gerada com Next.js e Three.js por @pedrorrosa",
 };
 
